8af0854c647529b249d4141f85d64ae21c2d889d
Leo Berg <leo.berg.8@gmail.com>
2025-11-28 12:47:45 +0100

    konsekvent hastighet

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,23 +2,23 @@
 using Raylib_cs;
 Raylib.InitWindow(800, 600, "Waa");
 Raylib.SetTargetFPS (60);
-//variables
+//variables and points
 Vector2 Spawn1 = new Vector2(150, 490);
 Vector2 Spawn2 = new Vector2(400, 490);
 Vector2 Spawn3 = new Vector2(650, 490);
-Vector2 ps = new();
-int Attack = 100;
-Vector2 end = new();
-int end2;
-List<Vector2> starts =[];
-List<Vector2> Ends =[];
-List<int> attackTimer = [];
+Vector2 ps = new(); // spawnpoint chosen for player attack
+List<Vector2> enemyspanw=[]; // where the enemy attacks starts
+List<Vector2> enemytarget =[]; // enemy attack end position
+List<Vector2> starts =[]; // start position for player attack
+List<Vector2> Ends =[]; //end position for player attack
+List<int> attackTimer = []; //how long a player attack has existed
+List<int> explosiontimmer = []; // how long a explosion has existed
+List<int> enemmyattacktimmer=[]; // how long an enemy attack timmer has existed
+int enemytimmer=0;  // time since an enemy has spawned
+int enemyallow=90; // when an enemy gets to spawn again by time
+int enemyallow2=1; // if an enemy can spawn
 while(!Raylib.WindowShouldClose()){
-
-    //player spawnpoints/
-    //player attacks(
-    //detect input
-if (Raylib.IsKeyPressed(KeyboardKey.Space))
+if (Raylib.IsKeyPressed(KeyboardKey.Space)) // what spawn point the player attack should launsh from
     {
         int S = Raylib.GetMouseX();
         if(S < 266)
@@ -34,81 +34,89 @@ if (Raylib.IsKeyPressed(KeyboardKey.Space))
             ps = Spawn3;
         }
     }
-    Vector2 mousePos = Raylib.GetMousePosition();
-    //pick spawnpoint
-    //line from spawn to input
-
-    //explosion at input
-
-    //)
-
-    //enemy attacks missiles(
-    //spawn point
-
-    //line towards a target
-
-    // if 
-    
-    //)
+    Vector2 mousePos = Raylib.GetMousePosition(); // where the player attack should target
+    enemytimmer ++;
+    if (enemytimmer==enemyallow && enemyallow2==1) {// determines if an enemy attack can spawn
+    int enemyspanwx =50+Random.Shared.Next(700); // detemines spawn position
+    Vector2 enemyspawn = new Vector2(enemyspanwx, 0);
+    enemyspanw.Add(enemyspawn);
+    //Console.WriteLine(enemyspawn);
+    enemmyattacktimmer.Add(0); // starts counting how long the enemy attack have existed
+    enemytimmer=0; // resets the timmer
+    enemyallow = 30+Random.Shared.Next(90); // determines time to enemy 
+    }
+    if(Raylib.IsKeyPressed(KeyboardKey.E) && enemyallow2 == 1) //allows to manually turn of enemy spawns
+    {
+        enemyallow2=0;
+    }
+    else if (Raylib.IsKeyPressed(KeyboardKey.E)&& enemyallow2 == 0) //allows to manually turn on enemy spawns
+    {
+        enemyallow2=1;
+    }
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.Black);
-    Raylib.DrawRectangle(0, 500, 800, 100, Color.Green);
-    Raylib.DrawCircle(150, 510, 60, Color.Green);
-    Raylib.DrawCircle(400, 510, 60, Color.Green);
-    Raylib.DrawCircle(650, 510, 60, Color.Green);
-    if (Raylib.IsKeyPressed(KeyboardKey.Space))
+    Raylib.DrawRectangle(0, 500, 800, 100, Color.Green); //terrain
+    Raylib.DrawCircle(150, 510, 60, Color.Green); // terrain
+    Raylib.DrawCircle(400, 510, 60, Color.Green); // terrain
+    Raylib.DrawCircle(650, 510, 60, Color.Green); // terrain
+    List<int> enemyremove=[];
+    for (int i = 0; i < enemyspanw.Count; i++) //enemy draw and add to delete
+    {
+        enemmyattacktimmer[i]++;
+        if (enemmyattacktimmer[i] > 300)
+        {
+            enemyremove.Add(i);
+        }
+        if (enemmyattacktimmer[i] < 300){
+      //Raylib.DrawLineEx(enemyspanw[i],-(enemyspanw[i] - enemytarget[i])*enemmyattacktimmer[i]/300+enemyspanw[i], 5, Color.Red);
+        }
+    }
+    for (int i = 0; i < enemyremove.Count; i++) //deleting finished enemy attack
+    {
+        enemmyattacktimmer.RemoveAt(enemyremove[i]);
+        enemyspanw.RemoveAt(enemyremove[i]);
+        //enemytarget.RemoveAt(enemyremove[i]);
+    }
+
+
+    if (Raylib.IsKeyPressed(KeyboardKey.Space))// determines player attack positions and timer
 
     {
         attackTimer.Add(0);
         starts.Add(ps);
         Ends.Add(mousePos);
-        
+        explosiontimmer.Add(0);
     }
-   // if (Attack < 45 || Raylib.IsKeyPressed(KeyboardKey.Space))
-  //  {
-  //      Vector2 start = ps;
-   //     Raylib.DrawLineEx(start, end, 5, Color.Blue);
-   //     Attack ++;
-   //     if (Attack > 30)
-   //     {
-   //         Raylib.DrawCircleV(end, 40, Color.Blue);
-   //     }
-   // }
 
-    List<int> toRemove = [];
-    for (int i = 0; i < starts.Count; i++)
+    List<int> toRemove = []; // player attacks to remove
+    for (int i = 0; i < starts.Count; i++) //player attacks to remove and draw
     {
         attackTimer[i]++;
-        if (attackTimer[i] > 45)
+        float Distance = Vector2.Distance(starts[i], Ends[i]);
+        if (attackTimer[i] > Distance/10+20)
         {
             toRemove.Add(i);
         }
-        if(attackTimer[i] <30){
-        float startsx=starts[i].X;
-        float startsy=starts[i].Y;
-        float endsx=Ends[i].X;
-        float endsy=Ends[i].Y;
-        float x= (endsx-startsx)*(attackTimer[i]/30);
-        float y= (endsy-startsx)*(attackTimer[i]/30);
-        Vector2 xy = new Vector2(x, y);
-        Raylib.DrawLineEx(starts[i],Ends[i], 5, Color.Blue);
-        Console.WriteLine(Ends[i]);
+        if(attackTimer[i] <Distance/10){ // line for the until target frames
+        Raylib.DrawLineEx(starts[i],-(starts[i] - Ends[i])*attackTimer[i]/(Distance/10)+starts[i], 5, Color.Blue);
 
         }
         else
-        {
+        {// line for the last 20 frames
             Raylib.DrawLineEx(starts[i],Ends[i], 5, Color.Blue);
         }
-        if (attackTimer[i] > 30)
-        {
-            Raylib.DrawCircleV(Ends[i], attackTimer[i]*2-40, Color.Blue);
+        if (attackTimer[i] > Distance/10)
+        { // explosion
+        explosiontimmer[i] ++;
+            Raylib.DrawCircleV(Ends[i], explosiontimmer[i]*2+20, Color.Blue);
         }
     }
-    for (int i = 0; i < toRemove.Count; i++)
+    for (int i = 0; i < toRemove.Count; i++)// removal of old variables for player attacks
     {
         attackTimer.RemoveAt(toRemove[i]);
         starts.RemoveAt(toRemove[i]);
         Ends.RemoveAt(toRemove[i]);
+        explosiontimmer.RemoveAt(toRemove[i]);
     }
 
 
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3925cae8601eb6f9df1beb527f62beae3f4a1e73")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
